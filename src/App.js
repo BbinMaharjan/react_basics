4c5775817f72014e9d0a26201531791f48cf9399
Bibin Maharjan <bbinmaharjan@gmail.com>
2021-01-29 20:48:55 +0545

    router added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import NavBar from "./components/Navbar";
+import Navbar from "./components/Navbar";
+import Posts from "./pages/Posts";
+import Users from "./pages/Users";
 
 function App() {
   return (
     <Router>
-      <NavBar />
+      <Navbar />
       <Switch>
-        <Route exact path='/posts' component></Route>
-        <Route exact path='/user' component></Route>
+        <Route exact path='/posts' component={Posts} />
+        <Route exact path='/users' component={Users} />
       </Switch>
     </Router>
   );
