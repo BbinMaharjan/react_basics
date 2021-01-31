24414bddf9d1ebb916715304c374804cd85bcf50
Bibin Maharjan <bbinmaharjan@gmail.com>
2021-01-31 22:13:44 +0545

    photo gallery added

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -1,8 +1,79 @@
 import React from "react";
+import axios from "axios";
+import {
+  Card,
+  Spinner,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardFooter,
+  CardSubtitle,
+} from "reactstrap";
 
+import "../pages/post.css";
+
+const BASE_URL = "https://jsonplaceholder.typicode.com";
 class Gallery extends React.Component {
+  state = {
+    photos: [],
+    isLoading: false,
+  };
+
+  componentDidMount = () => {
+    this.setState({
+      ...this.state,
+      isLoading: true,
+    });
+
+    axios
+      .get(`${BASE_URL}/photos`)
+      .then((res) => {
+        this.setState({
+          photos: res.data,
+          isLoading: false,
+        });
+      })
+      .catch((error) => {
+        this.setState({
+          ...this.state,
+          isLoading: false,
+        });
+      });
+  };
+
   render() {
-    return <h1>This is a Gallery Page</h1>;
+    if (this.state.isLoading)
+      return (
+        <div className='spinner'>
+          <Spinner
+            type='grow'
+            color='primary'
+            style={{ width: "5rem", height: "5rem" }}
+          />
+        </div>
+      );
+    return (
+      <div className='container'>
+        {this.state.photos.map((photo) => {
+          return (
+            <Card key={photo.id} className='mt-3'>
+              <CardBody>
+                <CardTitle> {photo.id} </CardTitle>
+                <CardText> {photo.url} </CardText>
+              </CardBody>
+              <CardFooter
+                style={{
+                  backgroundColor: "#e83e8c",
+                  textAlign: "center",
+                  color: "#fff",
+                }}>
+                <CardSubtitle> Image Title : {photo.title} </CardSubtitle>
+              </CardFooter>
+            </Card>
+          );
+        })}
+      </div>
+    );
   }
 }
 
